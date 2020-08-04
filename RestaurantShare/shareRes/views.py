a9bc0e7d4da676ed51635b4f6d3d19871b479d17
jana-choi <lovej0918@gmail.com>
2020-08-04 23:04:22 +0900

    1.3. Category CRUD

--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 # Create your views here.
 def index(request):
     # return HttpResponse("index")
-    return render(request, "shareRes/index.html")
+    categories = Category.objects.all()
+    content = {"categories": categories}
+    return render(request, "shareRes/index.html", content)
 
 def restaurantDetail(request):
     # return HttpResponse("restaurantDetail")
@@ -16,4 +20,20 @@ def restaurantCreate(request):
 
 def categoryCreate(request):
     # return HttpResponse("categoryCreate")
-    return render(request, "shareRes/categoryCreate.html")
+    categories = Category.objects.all()
+    content = {"categories": categories}
+    return render(request, "shareRes/categoryCreate.html", content)
+
+def Create_category(request):
+    # return HttpResponse("여기서 category Create 기능을 구현할 거야.")
+    category_name = request.POST["categoryName"]
+    new_category = Category(category_name = category_name)
+    new_category.save()
+    return HttpResponseRedirect(reverse("index"))
+    # return HttpResponseRedirect(reverse("cateCreatePage"))
+
+def Delete_category(request):
+    category_id = request.POST["categoryId"]
+    delete_category = Category.objects.get(id = category_id)
+    delete_category.delete()
+    return HttpResponseRedirect(reverse("cateCreatePage"))
