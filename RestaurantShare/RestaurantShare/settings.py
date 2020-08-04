570d78333bbf3c2ee9332e9a094a4d53d6b54ffa
jana-choi <lovej0918@gmail.com>
2020-08-04 21:54:35 +0900

    1.1 ~ 1.2. url & templates setting finish

--- RestaurantShare/RestaurantShare/settings.py
+++ RestaurantShare/RestaurantShare/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shareRes',
-    'sendEmail',
+    "shareRes",
+    "sendEmail",
 ]
 
 MIDDLEWARE = [
