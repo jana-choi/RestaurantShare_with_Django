11c6c108c47d66ffbca863fc258f2ce20782b08a
jana-choi <lovej0918@gmail.com>
2020-08-07 15:45:21 +0900

    2.4. send email result

--- RestaurantShare/sendEmail/views.py
+++ RestaurantShare/sendEmail/views.py
@@ -11,19 +11,23 @@ from django.template.loader import render_to_string
 
 # Create your views here.
 def sendEmail(request):
-    checked_res_list = request.POST.getlist("checks")
-    inputReceiver = request.POST["inputReceiver"]
-    inputTitle = request.POST["inputTitle"]
-    inputContent = request.POST["inputContent"]
-    restaurants = []
-    for checked_res_id in checked_res_list:
-        restaurants.append(Restaurant.objects.get(id = checked_res_id))
-    
-    content = {"inputContent": inputContent, "restaurants": restaurants}
+    try:
+        checked_res_list = request.POST.getlist("checks")
+        inputReceiver = request.POST["inputReceiver"]
+        inputTitle = request.POST["inputTitle"]
+        inputContent = request.POST["inputContent"]
+        restaurants = []
+        for checked_res_id in checked_res_list:
+            restaurants.append(Restaurant.objects.get(id = checked_res_id))
+        
+        content = {"inputContent": inputContent, "restaurants": restaurants}
 
-    msg_html = render_to_string("sendEmail/email_format.html", content)
+        msg_html = render_to_string("sendEmail/email_format.html", content)
 
-    msg = EmailMessage(subject=inputTitle, body=msg_html, from_email="id@gmail.com", bcc=inputReceiver.split(","))
-    msg.content_subtype = "html"
-    msg.send()
-    return HttpResponseRedirect(reverse("index"))
+        msg = EmailMessage(subject=inputTitle, body=msg_html, from_email="id@gmail.com", bcc=inputReceiver.split(","))
+        msg.content_subtype = "html"
+        msg.send()
+        return render(request, "sendEmail/sendSuccess.html")
+        # return HttpResponseRedirect(reverse("index"))
+    except:
+        return render(request, "sendEmail/sendFail.html")
